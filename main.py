1240524ba8f6af372bfe512294be0f3f177b5b05
K0G1408 <144092723+K0G1408@users.noreply.github.com>
2025-02-02 19:04:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import numpy as np
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import emotion_detection  # Tu archivo con el modelo de IA
+import emotion_detection
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
@@ -13,15 +13,8 @@ def predict():
     if 'audio' not in request.files:
         return jsonify({"error": "No audio file provided"}), 400
     
-    # Obtén el archivo correctamente
-    audio_file = request.files['audio']  # Esto debería ser un archivo binario
-    print(request.files)
-    print(f"Archivo recibido: {audio_file.filename}")
+    audio_file = request.files['audio']
     try:
-        # Cargar audio desde el archivo recibido
-        #audio_data, sr = librosa.load(BytesIO(audio_file.read()))
-        #print(f"Audio cargado: {audio_file.filename}")
-        
         # Llamar a la función que hace la predicción
         emotion = emotion_detection.make_prediction(audio_file)
         
